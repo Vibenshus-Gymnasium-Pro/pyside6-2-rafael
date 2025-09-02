0aa74657fe2f18c71b2d309410b0a4b09e7d50bf
NEXT15930 <fake@email.com>
2025-09-02 11:00:31 +0200

    Translation from normal language

--- roeversprog.py
+++ roeversprog.py
@@ -1,12 +1,19 @@
-# [[file:README.org::*Udvikling af logik][Udvikling af logik:1]]
 # Dette er et modul til oversættelse mellem almindelige sprog og røversprog
 
+consonants = "bcdfghjklmnpqrstvwxz"
+
 def oversaet_til_roeversprog(inputtekst):
-    outputtekst = "Denne funktion skal kunne oversætte til røversprog."
+    """ This function translates from nornal language."""
+    outputtekst = ""
+
+    for i in inputtekst:
+        if i.lower() not in consonants:
+            outputtekst += i
+        else:
+            outputtekst += f"{i}o{i}"
     return outputtekst
 
 
 def oversaet_fra_roeversprog_til_andet_sprog(inputtekst):
     outputtekst = "Her skal røversproget fjernes og almindeligt sprog skal returneres.\nGiv gerne fejlmeddelelser, hvis røversproget ikke er korrekt."
     return outputtekst
-# Udvikling af logik:1 ends here
